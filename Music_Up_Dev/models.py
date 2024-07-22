ddc0b4a764a86be8d615d4c0ef901b03d0e764a5
Shaban Dimitriy <shaban.benito.dimitri@gmail.com>
2024-07-23 01:36:47 +0300

    Artist_Post_Models_V01

--- Music_Up_Dev/models.py
+++ Music_Up_Dev/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Artist(models.Model):
+
+    nick_name = models.CharField((""), max_length=50)
+    password = models.CharField((""), max_length=50)
+    
+    email = models.EmailField((""), max_length=254)
+
+    #artist_post = models.ForeignKey("Post.Model", verbose_name=("art_post"), on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ("Artist")
+        verbose_name_plural = ("Artists")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+
+class Post(models.Model):
+
+    
+
+    class Meta:
+        verbose_name = ("")
+        verbose_name_plural = ("s")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
