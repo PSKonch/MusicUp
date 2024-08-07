f1c31d7119f219f19765ee9c5522b708135bee4f
Dimitriy Shaban <shaban.benito.dimitri@gmail.com>
2024-08-08 02:13:50 +0300

    Load_a_Song

--- Music_Up_Dev/urls.py
+++ Music_Up_Dev/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("Artists/", views.Artists_view, name="artists"),
     path("CreatePost/", views.Create_a_Post, name="create_post"),
-    path("Posts/", views.Posts_view, name="posts")
+    path("Posts/", views.Posts_view, name="posts"),
+    path("Songs/", views.Songs_view, name="song_list"),
+    path("LoadSong/", views.Load_a_Song, name="load_song"),
 ]
--- Music_Up_Dev/views.py
+++ Music_Up_Dev/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import CustomUser, Post_News, Song, Album
 from django.contrib.auth.decorators import login_required
-from .forms import PostCreateForm
+from .forms import LoadMusicForm, PostCreateForm
 
 # Create your views here.
 def index(request): #То же самое, что News
@@ -23,15 +23,27 @@ def Posts_view(request):
     return render(request, "Music_Up_Dev/posts.html", {'content': queryset})
 
 
-
+@login_required
 def Songs_view(request):
-    return HttpResponse("Songs List")
+    songs = Song.objects.filter(album__artist=request.user)
+    return render(request, 'Music_Up_Dev/song_list.html', {'songs': songs})
 
 
 
 @login_required
 def Load_a_Song(request):
-    return HttpResponse('Load a Song')
+    
+    if request.method == 'POST':
+        form = LoadMusicForm(request.POST, request.FILES, user=request.user)
+        
+        if form.is_valid(): 
+            form.save()
+            return HttpResponseRedirect(reverse('song_list'))
+        
+    else:
+        form = LoadMusicForm(user=request.user)
+    return render(request, 'Music_Up_Dev/load_song.html', {'form': form})
+
 
 
 
